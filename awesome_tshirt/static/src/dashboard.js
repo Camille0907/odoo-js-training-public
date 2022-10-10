9bb5de9c9f118d60cd50072529ef66cde9a8df67
Camille Morand <camille.morand@camptocamp.com>
2022-10-10 11:30:59 +0200

    1.2 A Todo Component

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -2,11 +2,17 @@
 
 import { registry } from "@web/core/registry";
 import { Counter } from "./counter";
+import { Todo } from "./todo";
 
 const { Component, useState } = owl;
 
 class AwesomeDashboard extends Component {
-    static components = { Counter };
+    static components = { Counter, Todo };
+
+    setup(){
+        this.todo1 = { id: 3, description: "Buy milk", done: false };
+        this.todo2 = { id: 3, description: "Buy eggs", done: true };
+    }
 }
 
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
---
+++ awesome_tshirt/static/src/todo.js
@@ -0,0 +1,10 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+
+const { Component, useState } = owl;
+
+export class Todo extends Component {
+
+}
+Todo.template = "awesome_tshirt.todo";
