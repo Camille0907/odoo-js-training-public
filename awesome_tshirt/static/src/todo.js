1e88030ff4f0f19c41da3ae5a2f1d2364309ded8
Camille Morand <camille.morand@camptocamp.com>
2022-10-10 11:47:40 +0200

    1.3 Props Validation

--- awesome_tshirt/static/src/todo.js
+++ awesome_tshirt/static/src/todo.js
@@ -5,6 +5,15 @@ import { registry } from "@web/core/registry";
 const { Component, useState } = owl;
 
 export class Todo extends Component {
-
+    static props = {
+        todo: {
+          type: Object,
+          shape: {
+            id: Number,
+            description: String,
+            done: Boolean,
+          }
+        }
+    }
 }
 Todo.template = "awesome_tshirt.todo";
